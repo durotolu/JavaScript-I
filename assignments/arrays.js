42cfc3fc59b052290675bd520624bf8d55b34959
Modurotolu Olokode <durolawk@gmail.com>
2019-08-19 23:16:00 +0100

    fixed JSON bug

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -121,4 +121,5 @@ let BMWAndAudi = [];
 for (let i=0; i<inventory.length; i++) {
   if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {BMWAndAudi.push(inventory[i])}
 }
-console.log(BMWAndAudi);
+let myJSON = JSON.stringify(BMWAndAudi);
+console.log(myJSON);
